379d4f478463e4f857b7bf445d6db026d8a3eb54
Liang Wang <ryanrhymes@gmail.com>
2019-04-05 09:45:00 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,14 @@
 
 echo "Iving Applet Example"
 
+echo "This demonstrate the applet can be configured properly."
+
 ping ${PINGHOST} &
+
+echo "This demostrates the image can be rebuilt properly."
 
 while [ true ]
 do
-  echo 'pear'
   echo "ENV_VAR=${PINGHOST}"
   sleep 10
 done
