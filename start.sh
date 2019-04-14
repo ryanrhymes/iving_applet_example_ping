729c4e18a4e043ffbe0ad10e47f669d055ca27e5
Liang Wang <ryanrhymes@gmail.com>
2019-04-14 23:26:13 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,6 +10,6 @@ echo "This demostrates the image can be rebuilt properly."
 
 while [ true ]
 do
-  echo "ENV_VAR=${PINGHOST}, IVING_DEVICE_HOSTNAME=${IVING_DEVICE_HOSTNAME}"
+  echo "ENV_VAR=${PINGHOST}, IVING_DEVICE_HOSTNAME=${IVING_DEVICE_HOSTNAME}, TEST_VAR=${TEST_VAR}"
   sleep 10
 done
